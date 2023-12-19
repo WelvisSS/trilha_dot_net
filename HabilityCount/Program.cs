7f46293577a5d1d30918d8d3da72d543c608733a
Eduardo Matheus <eduardomatheusuesc@gmail.com>
2023-12-18 21:08:01 -0300

    Create Program.cs

--- HabilityCount/Program.cs
+++ HabilityCount/Program.cs
@@ -1,1 +1,9 @@
+using HabilityCount;
 
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/eduardo/", () => Eduardo.View());
+
+app.Run();
