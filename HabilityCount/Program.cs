e4c859229d38b86e66a24957497d910b24d9bef3
giuseppemota <75266680+giuseppemota@users.noreply.github.com>
2023-12-18 21:02:09 -0300

    Update Program.cs

--- HabilityCount/Program.cs
+++ HabilityCount/Program.cs
@@ -1,1 +1,10 @@
+using Roadmap_ASP_NET_CORE;
 
+//Onde tiver DevName, substitua pelo seu nome;
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/Giuseppe/", () => Giuseppe.View());
+
+app.Run();
