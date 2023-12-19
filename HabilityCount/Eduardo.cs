baa270689587b3ebff89e7d7ba3234a9fb67bbbb
Welvis <welvisconta@gmail.com>
2023-12-18 21:43:05 -0300

    branch devWelvis

--- HabilityCount/Eduardo.cs
+++ HabilityCount/Eduardo.cs
@@ -2,16 +2,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Text;
 
-namespace HabilityCount
+
+namespace HabilityCount;
+
+public static class Eduardo
 {
-    public class static Eduardo
-    {
-        public static string Name => "Eduardo Matheus de Menezes Souto";
+      public static string Name => "Eduardo Matheus de Menezes Souto";
       public static List<(string, int)> Skills => new List<(string, int)>{
             ("Fundamentos de C#", 5),
             ("Habilidades Gerais de Desenvolvimento",5),
-            ("Fundamentos de Banco de Dados",5)("Fundamentos de C#", 4),
+            ("Fundamentos de Banco de Dados",5),
+            ("Fundamentos de C#", 4),
             ("Habilidades Gerais de Desenvolvimento", 4),
             ("Fundamentos de Banco de Dados", 4),
             ("Fundamentos basicos de ASP.NET Core", 2),
@@ -33,12 +36,14 @@ namespace HabilityCount
             ("Bibliotecas adicionais", 1)
 
          };
-      public static string View(){
+      public static string View()
+      {
             var sb = new StringBuilder();
             sb.AppendLine($"Nome: {Name}");
             sb.AppendLine();
             sb.AppendLine("Habilidades:");
-            foreach(var skill in Skills){
+            foreach (var skill in Skills)
+            {
                   sb.AppendLine($"\t{skill.Item1} - {skill.Item2} estrelas");
             }
             var sum = Skills.Sum(x => x.Item2);
@@ -46,5 +51,4 @@ namespace HabilityCount
             sb.AppendLine($"Total de estrelas: {sum}");
             return sb.ToString();
       }
-    }
 }
--- HabilityCount/Program.cs
+++ HabilityCount/Program.cs
@@ -5,5 +5,9 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 app.MapGet("/eduardo/", () => Eduardo.View());
+app.MapGet("/matheus>/", () => Matheus.View());
+app.MapGet("/giuseppe/", () => Giuseppe.View());
+app.MapGet("/welvis/", () => Welvis.View());
+
 
 app.Run();
