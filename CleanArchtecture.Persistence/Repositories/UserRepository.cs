28ac5870340607b762337aa97509037fb0bce3df
Eduardo Matheus <eduardomatheusuesc@gmail.com>
2024-01-07 15:56:06 -0300

    Finish UnitOfWork

--- CleanArchtecture.Persistence/Repositories/UserRepository.cs
+++ CleanArchtecture.Persistence/Repositories/UserRepository.cs
@@ -1,12 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using CleanArchtecture.Domain.Entities;
+using CleanArchtecture.Domain.Interfaces;
+using CleanArchtecture.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
 
-namespace CleanArchtecture.Persistence.Repositories
+namespace CleanArchtecture.Persistence.Repositories;
+public class UserRepository : BaseRepository<User>, IUserRepository
 {
-    public class UserRepository
+    public UserRepository(AppDbContext context) : base(context)
+    { }
+
+    public async Task<User> GetByEmail(string email, CancellationToken cancellationToken)
     {
-        
+        return await Context.Users.FirstOrDefaultAsync(x => x.Email == email, cancellationToken);
     }
 }
