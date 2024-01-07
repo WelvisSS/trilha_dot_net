f388356a5876b3e44298563fa7751e4d00ddffb1
Eduardo Matheus <eduardomatheusuesc@gmail.com>
2024-01-07 15:59:52 -0300

    Finish id03

--- CleanArchtecture.Persistence/Context/AppDbContext.cs
+++ CleanArchtecture.Persistence/Context/AppDbContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using CleanArchtecture.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- CleanArchtecture.Persistence/ServiceExtensions.cs
+++ CleanArchtecture.Persistence/ServiceExtensions.cs
@@ -1,12 +1,21 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using CleanArchtecture.Domain.Interfaces;
+using CleanArchtecture.Persistence.Context;
+using CleanArchtecture.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
-namespace CleanArchtecture.Persistence
+namespace CleanArchtecture.Persistence;
+public static class ServiceExtensions
 {
-    public class ServiceExtensions
+    public static void ConfigurePersistenceApp(this IServiceCollection services,
+                                                IConfiguration configuration)
     {
-        
+        var connectionString = configuration.GetConnectionString("Sqlite");
+        services.AddDbContext<AppDbContext>(opt => opt.UseSqlite(connectionString));
+
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        services.AddScoped<IUserRepository, UserRepository>();
+
     }
 }
