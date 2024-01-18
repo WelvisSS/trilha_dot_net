0a95dca5c2e543076008f9601a144b789932eece
EduardoMatheus96 <eduardomatheusuesc@gmail.com>
2024-01-17 23:52:02 -0300

    Last Migration One To Many

---
+++ BarberApp/BaberApp.Persistence/Migrations/20240118025005_EmployeeToServices_Migrations.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BaberApp.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class EmployeeToServices_Migrations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ServiceId",
+                table: "Employeess",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Services_EmployeeId",
+                table: "Services",
+                column: "EmployeeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Services_Employeess_EmployeeId",
+                table: "Services",
+                column: "EmployeeId",
+                principalTable: "Employeess",
+                principalColumn: "EmployeeId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Services_Employeess_EmployeeId",
+                table: "Services");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Services_EmployeeId",
+                table: "Services");
+
+            migrationBuilder.DropColumn(
+                name: "ServiceId",
+                table: "Employeess");
+        }
+    }
+}
