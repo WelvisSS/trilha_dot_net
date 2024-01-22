deff5f1a9760e80579ef93fd6b5905ef97e523ff
Wilton Freire <wilton.lio@gmail.com>
2024-01-22 20:11:19 -0300

    Enviando ID05 CRUD e Migrations

--- ResTIConnect/ResTIConnect.Infrastructure/Context/ResTIConnectContext.cs
+++ ResTIConnect/ResTIConnect.Infrastructure/Context/ResTIConnectContext.cs
@@ -13,7 +13,7 @@ public class ResTIConnectContext : DbContext
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         base.OnConfiguring(optionsBuilder);
-        var connectionString = "server=localhost;user=root;password=root;database=resticonnect";
+        var connectionString = "server=localhost;user=root;password=0000;database=resticonnect";
         var serverVersion = ServerVersion.AutoDetect(connectionString);
 
         optionsBuilder.UseMySql(connectionString, serverVersion);
