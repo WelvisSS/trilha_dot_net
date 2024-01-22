d9fc39083bc2c5241eada032dfffc6a28b4bfc0a
EduardoMatheus96 <eduardomatheusuesc@gmail.com>
2024-01-22 12:31:11 -0300

    Esqueci de mudar a senha do banco

--- ResTIConnect/ResTIConnect.Infrastructure/Context/ResTIConnectContext.cs
+++ ResTIConnect/ResTIConnect.Infrastructure/Context/ResTIConnectContext.cs
@@ -12,7 +12,7 @@ public class ResTIConnectContext : DbContext
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         base.OnConfiguring(optionsBuilder);
-        var connectionString = "server=localhost;user=root;password=432747;database=resticonnect";
+        var connectionString = "server=localhost;user=root;password=root;database=resticonnect";
         var serverVersion = ServerVersion.AutoDetect(connectionString);
 
         optionsBuilder.UseMySql(connectionString, serverVersion);
