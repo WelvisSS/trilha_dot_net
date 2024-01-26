8a81c685f2d683ad0d8be533afc15df257f9418c
EduardoMatheus96 <eduardomatheusuesc@gmail.com>
2024-01-26 10:08:07 -0300

    Tentando entender como criar a pessoa fisica e juridica

---
+++ BarberApp/BaberApp.Persistence/Data/ClientDB.cs
@@ -0,0 +1,53 @@
+﻿using BarberApp.Domain.Entities;
+using BarberApp.Domain.Interfaces;
+
+namespace BaberApp.Persistence.Data;
+
+public class ClientDB : IClientCollection
+{
+    private readonly List<Client> _clients = new List<Client>();
+    private int _id = 0;
+    public void Create(Client client)
+    {
+        if (_clients.Count > 0)
+            _id = _clients.Max(m => m.ClientId);
+        client.ClientId = ++_id;
+        client.PersonId = ++_id;
+        LegalPerson legalPerson = new LegalPerson
+        {
+            PersonId = client.PersonId,
+            CNPJ = "11111111111111",
+        };
+         PhysicalPerson physicalPerson = new PhysicalPerson
+        {
+            PersonId = client.PersonId,
+            CPF = "00000000000000",
+        };
+        _clients.Add(client);
+    }
+
+    public void Delete(int id)
+    {
+        _clients.RemoveAll(m => m.ClientId == id);
+    }
+
+    public ICollection<Client> GetAll()
+    {
+        return _clients.ToArray();
+    }
+
+    public Client? GetById(int id)
+    {
+        return _clients.FirstOrDefault(m => m.ClientId == id);
+    }
+
+    public void Update(int id, Client client)
+    {
+        var clientDB = _clients.FirstOrDefault(m => m.ClientId == id);
+        if (clientDB is not null)
+        {
+            clientDB.Name = client.Name;
+            clientDB.Email = client.Email;
+        }
+    }
+}
