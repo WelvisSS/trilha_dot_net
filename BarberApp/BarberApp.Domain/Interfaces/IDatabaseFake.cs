7067cc1ea8670b2a55bc67e67a544c0023e75716
GiuseppeMota <giuseppemota@gmail.com>
2024-01-26 11:43:49 -0300

    create estimate controller and depedences

--- BarberApp/BarberApp.Domain/Interfaces/IDatabaseFake.cs
+++ BarberApp/BarberApp.Domain/Interfaces/IDatabaseFake.cs
@@ -4,4 +4,5 @@ public interface IDatabaseFake
 {
     public IRequestCollection RequestsCollection { get; }
     public IEmployeeCollection EmployeesCollection { get; }
+    public IServiceCollection ServicesCollection { get; }
 }
