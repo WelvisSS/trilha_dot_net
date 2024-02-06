acbd561fe7605d082fabe5a687d749770f9f38e2
EduardoMatheus96 <eduardomatheusuesc@gmail.com>
2024-02-06 08:44:07 -0300

    Creating Patient Service

---
+++ TechMed.Application/Services/BaseService.cs
@@ -0,0 +1,11 @@
+﻿using TechMed.Infrastructure.Persistence;
+
+namespace TechMed.Application;
+
+public abstract class BaseService
+{
+    public readonly TechMedDbContext _context;
+    protected BaseService(TechMedDbContext context){
+        _context = context;
+    }
+}
---
+++ TechMed.Application/Services/Interfaces/IBaseService.cs
@@ -0,0 +1,8 @@
+﻿namespace TechMed.Application;
+
+public interface IBaseService <TViewModel, TInputModel>
+where TViewModel : class
+where TInputModel : class
+{
+
+}
