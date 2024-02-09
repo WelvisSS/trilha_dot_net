d2b7f84e852508a0b837b4b687f3937bea7f4161
Welvis <welvisconta@gmail.com>
2024-02-09 09:30:14 -0300

    Reverter pull request 3842bf69fe6b2a32674592ecff864305eb247739

--- ResTIConnect/ResTIConnect.Aplication/InputModels/NewPerfilInputModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ResTIConnect.Aplication.InputModels;
-
-public class NewPerfilInputModel
-{
-    public int PerfilId { get; set; }
-    public string? Descricao { get; set; }
-    public string? Permissoes { get; set; }
-}
-
--- ResTIConnect/ResTIConnect.Domain/Interfaces/IUserRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using ResTIConnect.Domain.Entities;
-
-namespace ResTIConnect.Domain.Interfaces;
-
-public interface IUserRepository<T> where T : User
-{
-   Task<User> GetUsuarioNome(string Nome);
-}
