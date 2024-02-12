1f79ebba0b061248deb7d844dda4bef32476c91a
EduardoMatheus96 <eduardomatheusuesc@gmail.com>
2024-02-12 00:53:55 -0300

    Creating CRUD for Client

---
+++ BarberApp.Application/ViewModels/ClientViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BarberApp.Application.ViewModels;
+
+public class ClientViewModel
+{
+    public required int ClientId { get; set; }
+    public required int PersonId {get; set;}
+    public required string Name { get; set; }
+    public required string Email { get; set; }
+    public required string CEP { get; set; }
+}
