c920f5a8541c8ff805fb11ab83f7ce81b14bb3a1
EduardoMatheus96 <eduardomatheusuesc@gmail.com>
2024-02-26 15:49:37 -0300

    Creating AuthService

---
+++ ResTIConnect/ResTIConnect.Infrastructure/Auth/IAuthService.cs
@@ -0,0 +1,7 @@
+﻿namespace ResTIConnect.Infrastructure.Auth;
+
+public interface IAuthService
+{
+    // string GenerateJwtToken(string email, string role);
+    string ComputeSha256Hash(string pass);
+}
