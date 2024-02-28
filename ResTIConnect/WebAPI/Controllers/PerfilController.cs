8e78043ff969e3606002d35b2f80aee4201b4982
Welvis <welvisconta@gmail.com>
2024-02-28 00:10:24 -0300

    finish

--- ResTIConnect/WebAPI/Controllers/PerfilController.cs
+++ ResTIConnect/WebAPI/Controllers/PerfilController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using ResTIConnect.Aplication.InputModels;
 using ResTIConnect.Aplication.Services.Interfaces;
 using ResTIConnect.Aplication.ViewModels;
@@ -7,6 +8,7 @@ namespace ResTIConnect.WebAPI.Controllers;
 
 [ApiController]
 [Route("/api/v0.1/")]
+[Authorize]
 public class PerfilController : ControllerBase
 {
     private readonly IPerfilService _perfilService;
--- ResTIConnect/WebAPI/Controllers/SistemaController.cs
+++ ResTIConnect/WebAPI/Controllers/SistemaController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using ResTIConnect.Aplication;
 using ResTIConnect.Aplication.InputModels;
 using ResTIConnect.Aplication.ViewModels;
@@ -7,6 +8,7 @@ namespace ResTIConnect.WebAPI.Controllers;
 
 [ApiController]
 [Route("/api/v0.1/")]
+[Authorize]
 public class SistemaController : ControllerBase
 {
     private readonly ISistemaService _sistemaService;
